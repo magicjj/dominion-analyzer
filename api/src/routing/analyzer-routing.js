4b0a864f5c3d3de1b4c490297f74585286ea59c6
Jake Ensor <magicjj@gmail.com>
2017-05-11 09:20:30 -0400

    test routing thing

--- api/src/routing/analyzer-routing.js
+++ api/src/routing/analyzer-routing.js
@@ -4,19 +4,17 @@ class Routes {
 	init(server) {
 		server.post('/analyze',
 			function(req, res, next) {
-				let buffer = new Buffer(10);
-				res.send(200, buffer);
 				DominionAnalyzer.parse(req.body.input)
 					.then(
 						(gdo) => {
-							buffer.write("TEST");
+							res.send(gdo);
+							next();
 						},
 						(err) => {
 							throw err;
 						}
 					)
 				;
-				return next();
 			}
 		);
 	}
