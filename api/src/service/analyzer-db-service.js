a6a528fe363294b0114d6daece4f4187978c1958
Jake Ensor <magicjj@gmail.com>
2017-05-22 18:30:07 -0400

    changing mongodb url

--- api/src/service/analyzer-db-service.js
+++ api/src/service/analyzer-db-service.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 const Promise = require('promise');
 
-const url = 'mongodb://admin:tickletickle@138.197.99.196:27017/windominion';
+const url = 'mongodb://138.197.99.196:27017/windominion';
 
 const makeKey = function() {
     let text = "";
@@ -19,70 +19,78 @@ class AnalyzerDbService {
     addGame(gameLog, gameData) {
         return new Promise((resolve, reject) => {
             let _resolve = resolve, _reject = reject;
-            this._connect((db, callback) => {
-                let add = (thisKey) => {
-                    gameData.key = thisKey;
-                    db.collection('gameData').insertOne(
-                        {
-                            createTime: Date.now(),
-                            gameLog,
-                            gameData,
-                            key: thisKey
-                        },
-                        (err, result) => {
-                            if (err) _reject(err);
-                            console.log("inserted record");
-                            _resolve(result);
-                        }
-                    );
-                }
-                let findUniqueKeyThenAdd = () => {
-                    let thisKey = makeKey();
-                    this.getGame(thisKey)
-                        .then(
-                            dupCheckRes => {
-                                if (dupCheckRes === null) {
-                                    add(thisKey);
-                                } else {
-                                    findUniqueKeyThenAdd();
-                                }
+            try {
+                this._connect((db, callback) => {
+                    let add = (thisKey) => {
+                        gameData.key = thisKey;
+                        db.collection('gameData').insertOne(
+                            {
+                                createTime: Date.now(),
+                                gameLog,
+                                gameData,
+                                key: thisKey
                             },
-                            err => {
-                                if (err) reject(err);
+                            (err, result) => {
+                                if (err) _reject(err);
+                                console.log("inserted record");
+                                _resolve(result);
                             }
-                        )
-                    ;
-                };  // end findUniqueKeyThenAdd fn dec
-                findUniqueKeyThenAdd();
-            }); // end _connect callback
+                        );
+                    }
+                    let findUniqueKeyThenAdd = () => {
+                        let thisKey = makeKey();
+                        this.getGame(thisKey)
+                            .then(
+                                dupCheckRes => {
+                                    if (dupCheckRes === null) {
+                                        add(thisKey);
+                                    } else {
+                                        findUniqueKeyThenAdd();
+                                    }
+                                },
+                                err => {
+                                    if (err) reject(err);
+                                }
+                            )
+                        ;
+                    };  // end findUniqueKeyThenAdd fn dec
+                    findUniqueKeyThenAdd();
+                }); // end _connect callback
+                } catch (e) {
+                reject(e);
+            }
         }); // end return new promise
     }
 
     getGame(key) {
         return new Promise((resolve, reject) => {
-            this._connect((db, callback) => {
-                db.collection('gameData').findOne(
-                    {
-                        key
-                    },
-                    (err, result) => {
-                        if (err) reject(err);
-                        if (result === null) {
-                            console.log("record not found");
-                        } else {
-                            console.log("record found");
+            try {
+                this._connect((db, callback) => {
+                    db.collection('gameData').findOne(
+                        {
+                            key
+                        },
+                        (err, result) => {
+                            if (err) reject(err);
+                            if (result === null) {
+                                console.log("record not found");
+                            } else {
+                                console.log("record found");
+                            }
+                            resolve(result);
                         }
-                        resolve(result);
-                    }
-                );
-            })
+                    );
+                })
+            } catch (e) {
+                reject(e);
+            }
         });
     }
 
     _connect(callback) {
         return MongoClient.connect(url, function(err, db) {
             if (err !== null) {
-                reject(err);
+                throw err;
             }
             callback(db, function() {
                 db.close();
